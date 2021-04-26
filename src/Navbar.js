d415ed40a123bc4fb64b19c2a6d8b77a4759a32a
John <suttontaj13@gmail.com>
2021-04-26 15:04:31 -0400

    Use HashRouter instead of BrowserRouter

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ import Error from './Error'
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography} from '@material-ui/core';
 
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   appBarOption: {
@@ -35,18 +35,18 @@ function Navbar() {
               Vigenère Variants Tool
             </Typography>
             <Typography className={classes.appBarOption} variant="h6" color="inherit">
-              <Link className={classes.a} to="/vigenere-variants-tool">Encipher / Decipher</Link>
+              <Link className={classes.a} to="/">Encipher / Decipher</Link>
             </Typography>
             <Typography className={classes.appBarOption} variant="h6" color="inherit">
-              <Link className={classes.a} to="/vigenere-variants-tool/solver">Solver</Link>
+              <Link className={classes.a} to="/solver">Solver</Link>
             </Typography>
           </Toolbar>
         </AppBar>
 
         <Switch>
-          <Route exact path="/vigenere-variants-tool/" component={App}/>
-          <Route exact path="/vigenere-variants-tool/solver" component={Solver}/>
-          <Route path="/vigenere-variants-tool/*" component={Error}/>
+          <Route exact path="/" component={App}/>
+          <Route exact path="/solver" component={Solver}/>
+          <Route path="/*" component={Error}/>
         </Switch>
       </Router>
     )
